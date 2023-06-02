e0ca57505a3784bbba59daac2d19fe160062ae3b
Arthur Machado <103700352+Arthurm12@users.noreply.github.com>
2023-06-02 17:05:53 -0300

    Update home.js
    Atualização pagina Home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { Appbar } from 'react-native-paper';
-import { useNavigation } from '@react-navigation/native';
-
-import Container from '../components/container';
-import Cards from '../components/cardsHome';
-import Banner from '../components/banner';
+import React from "react";
+import Carousels from "../components/carousels";
+import Container from "../components/container";
+import Cards from "../components/cards";
+import Header from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
+import { ScrollView } from "react-native";
 
 
 const Home = () => {
   const navigation = useNavigation();
   return (
     <Container>
-      <Banner />
-      <Cards />
+      <Header>
+        <ScrollView>
+          <Carousels />
+          <Cards
+            nome={"Painel"}
+            icon={"airballoon-outline"}
+            rota={() => navigation.navigate("Painel")}
+          />
+          <Cards
+            nome={"Pontos Turisticos"}
+            icon={"airballoon-outline"}
+            rota={() => navigation.navigate("PontosTuristicos")}
+          />
+          <Cards
+            nome={"Onde Dormir"}
+            icon={"bed-outline"}
+            rota={() => navigation.navigate("OndeDormir")}
+          />
+          <Cards
+            nome={"Onde Comer"}
+            icon={"silverware-fork-knife"}
+            rota={() => navigation.navigate("OndeComer")}
+          />
+          <Cards
+            nome={"Lojas"}
+            icon={"shopping-outline"}
+            rota={() => navigation.navigate("Lojas")}
+          />
+          <Cards
+            nome={"Serviços Publicos"}
+            icon={"account-search-outline"}
+            rota={() => navigation.navigate("Servicos")}
+          />
+        </ScrollView>
+      </Header>
     </Container>
   );
 };
+
 export default Home;
